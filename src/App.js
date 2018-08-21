5e26b27e3d9607cf1e41fc6b8620d527a1a0dd0a
krossmaskinen <bill.kurtson@gmail.com>
2018-08-21 16:21:33 +0200

    - more login test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Awesome stuff here!!</h1>
+        <h1>Awesome stuff here!!!</h1>
       </div>
     );
   }
