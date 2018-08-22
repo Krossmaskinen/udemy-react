5a6e9bb2b4354ba5781c33ebe2e82c77f36ad6f8
krossmaskinen <bill.kurtson@gmail.com>
2018-08-22 10:54:23 +0200

    - change names on button click in App

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Bill', age: 31 },
+      { name: 'Fabian', age: 26 },
+      { name: 'Kei', age: 37 }
+    ]
+  }
+
+  switchNameHandler = () => {
+     // DON'T DO THIS: this.state.persons[0].name = "Billiam";
+     this.setState({persons: [
+      { name: 'Billiam', age: 31 },
+      { name: 'Fabian the fit', age: 26 },
+      { name: 'Kei the krosser', age: 37 }
+    ]})
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Awesome stuff here!!!</h1>
-        <Person name="Bill" age="31" />
-        <Person name="Fabian" age="26">My Hobbies: Milruck</Person>
-        <Person name="Kei" age="37" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
