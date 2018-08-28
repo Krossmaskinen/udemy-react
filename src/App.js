9982572afee00919e138b361a3bcd18ba8bbf8d0
Bill Kurtson <bill.kurtson@gmail.com>
2018-08-28 11:30:57 +0200

    - add missing changes in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -46,15 +47,18 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                name={person.name}
-                age={person.age}
-                click={() => {this.deletePersonHandler(index);}}
-                changed={(event) => {this.nameChangedHandler(event, person.id)}}
+              <ErrorBoundary
                 key={person.id}
               >
-                My Hobbies: Milruck  
-              </Person>
+                <Person
+                  name={person.name}
+                  age={person.age}
+                  click={() => {this.deletePersonHandler(index);}}
+                  changed={(event) => {this.nameChangedHandler(event, person.id)}}
+                >
+                  My Hobbies: Milruck  
+                </Person>
+              </ErrorBoundary>
             );
           })}
         </div>
