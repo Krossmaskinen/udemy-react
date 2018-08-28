6ed6955b724e2b9a074c32b257a6cd5a5801d75f
Bill Kurtson <bill.kurtson@gmail.com>
2018-08-28 13:17:09 +0200

    - bugfix: wrong ErrorBoundary component naming

--- src/ErrorBoundary/ErrorBoundary.js
+++ src/ErrorBoundary/ErrorBoundary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class errorBoundary extends Component {
+class ErrorBoundary extends Component {
     state = {
         hasError: false,
         errorMessage: ''
