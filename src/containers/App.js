4035100b6b1d9eaceb5381a26284a9bc4f599f27
Bill Kurtson <bill.kurtson@gmail.com>
2018-08-30 09:17:47 +0200

    - remake WithClass to a wrapping component, withClass - use withClass for Person and App

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../components/hoc/WithClass';
+import Aux from '../components/hoc/Aux';
+import withClass from '../components/hoc/withClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -73,7 +74,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.appTitle}
@@ -82,9 +83,9 @@ class App extends PureComponent {
           clicked={this.togglePersonsHandler}
         />
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
